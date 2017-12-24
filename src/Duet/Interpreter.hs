315265e58478426d34db716d25b970ed473aea3b
John Evans <lgastako@gmail.com>
2017-12-24 01:20:57 -0500

    Logging removed by still IO.

--- src/Duet/Interpreter.hs
+++ src/Duet/Interpreter.hs
@@ -29,9 +29,6 @@ import           Duet.Reference                  ( Reference( RegisterRef
                                                  )
 import           Duet.RegisterId                 ( RegisterId )
 import           Duet.Value                      ( Value( Value ) )
-import           System.IO                       ( hFlush
-                                                 , stdout
-                                                 )
 
 data InterpreterState = InterpreterState
   { lastSound          :: Maybe Value
@@ -62,7 +59,7 @@ interpret s = execute instructions'
 execute :: [Instruction] -> IO Text
 execute ins = do
   (val, _) <- execute' ins
-  return $ "\n\n\nFinal value: " <> show val <> "\n\n"
+  return . show $ val
 
 execute' :: MonadIO m => [Instruction] -> m (Maybe Value, InterpreterState)
 execute' ins = runStateT executeAll $ (fromInstructions ins)
@@ -82,23 +79,12 @@ fromInstructions instructions' = InterpreterState
 executeAll :: MonadIO m => StateT InterpreterState m (Maybe Value)
 executeAll = do
   void . runExceptT . forever $ do
-    lift $ log ""
-    lift $ log "================================================================"
-    lift $ log ""
-
-    get >>= lift . log . ("BEFORE STATE: " <>) . show
-
     lift executeOne
-
-    lift $ log ""
-    get >>= lift . log . ("AFTER STATE: " <>) . show
 
     s <- get
     when (terminated s) $ do
-      lift $ log "STOPPING"
       stop ()
 
-  log "BEFORE lastsound"
   lastSound <$> get
 
 executeOne :: MonadIO m => StateT InterpreterState m ()
@@ -107,18 +93,11 @@ executeOne = do
   case atMay (instructions s) (instructionPointer s) of
     Nothing -> panic "THE FRONT FELL OFF" -- TODO normal termination
     Just instruction -> do
-      log . ("EXECUTING INS:  " <>) . show $ instruction
       step instruction
-      log "Instruction EXECUTED, updating IP."
       case instruction of
         Jgz x' _ -> getReferenceValue x' >>= \x -> if x > 0 then return () else jmp 1
         _        -> jmp 1
 
-      -- TODO: get rid of this:
-      -- modify (stashRegisters instruction)
-      st' <- get
-      log . ("IP AFTER EXECUTION:   " <>) . show $ instructionPointer st'
-      log . ("UPDATED REGISTERS: " <>) . show $ registers st'
 
 step :: MonadIO m => Instruction -> StateT InterpreterState m ()
 
@@ -130,7 +109,6 @@ step (Add reg ref) = do
 step (Jgz ref0 ref1) = do
   x <- getReferenceValue ref0
   y <- getReferenceValue ref1
-  log $ "jgz: " <> show (x, y, x > 0)
   if x > 0
     then jmp y
     else return ()
@@ -159,17 +137,10 @@ step (Snd ref) = do
 
 jmp :: MonadIO m => Value -> StateT InterpreterState m ()
 jmp (Value offset) = do
-  -- TODO
-  s' <- get
-  log $ "jmp: " <> show offset
-  log $ "instructionPointer pre-jmp: " <> (show $ instructionPointer s')
-
   modify updatePointer
   s <- get
-  log $ "updated pointer: " <> (show $ instructionPointer s) -- TODO
   when (outOfBounds s) $ do
     modify terminate
-    log "should be also TERMINATED"
   where
     updatePointer :: InterpreterState -> InterpreterState
     updatePointer st = st { instructionPointer = instructionPointer st + offset }
@@ -196,31 +167,3 @@ getRegisterValue regId = get >>= \st -> return $ Map.findWithDefault 0 regId (re
 getReferenceValue :: MonadIO m => Reference -> StateT InterpreterState m Value
 getReferenceValue (RegisterRef r) = getRegisterValue r
 getReferenceValue (ValueRef v)    = return v
-
-
--- ================================================================
--- TODO: get rid of this:
--- ================================================================
-
--- stashRegisters :: Instruction -> InterpreterState -> InterpreterState
--- stashRegisters instruction st = st { traces = traces' }
---   where
---     traces' = show (instruction, ip, registers st) <> "\n" <> traces st
---     ip = instructionPointer st
-
--- logRegisters :: MonadIO m  => Instruction -> StateT InterpreterState m ()
--- logRegisters ins = do
---   st <- get
---   log ""
---   log . ("INS:  " <>) . show $ ins
---   log . ("IP:   " <>) . show $ instructionPointer st
---   log . ("REGS: " <>) . show $ registers st
-
--- renderTrace :: (Instruction, Int, Map RegisterId Value) -> Text
--- renderTrace (ins', ip, regs) = ""
---   <> " INS: "  <> show ins'
---   <> " IP: "   <> show ip
---   <> " REGS: " <> show regs
-
-log :: MonadIO m => Text -> StateT InterpreterState m ()
-log s = liftIO $ putLn s >> hFlush stdout
